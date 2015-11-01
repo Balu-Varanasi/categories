85b1f06f4cf60fbf14ba59ecfe35b7e94b8026c5
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-11-01 12:42:48 +0530

    add :with_images trait to product model

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,4 +2,10 @@ FactoryGirl.define do
   factory :product do |f|
     f.name { Faker::Commerce.product_name }
   end
+
+  trait :with_images do
+    after(:build) do |product|
+      create_list(:image, 2, parent: product)
+    end
+  end
 end
